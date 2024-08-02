af32630fdc8bc100c0c08919acf21c857d6dbcf1
Justin Tingley <Jtingley2021@gmail.com>
2024-08-02 09:55:45 -0500

    IWeatherForecastService Interface

--- Restaurants.API/Controllers/WeatherForecastController.cs
+++ Restaurants.API/Controllers/WeatherForecastController.cs
@@ -6,28 +6,23 @@ namespace Restaurants.API.Controllers;
 [Route("api/[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+    
 
     private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IWeatherForecastService _weatherForecastService;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, 
+        IWeatherForecastService weatherForecastService)
     {
         _logger = logger;
+        _weatherForecastService = weatherForecastService;
     }
 
     [HttpGet]
     [Route("example")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        var result = _weatherForecastService.Get();
+        return result;
     }
 }
