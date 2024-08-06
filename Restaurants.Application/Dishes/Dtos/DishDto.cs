f2489f150428dfedcd5f428571e33faa8a169d74
Justin Tingley <Jtingley2021@gmail.com>
2024-08-06 16:17:06 -0500

    Dto objects

---
+++ Restaurants.Application/Dishes/Dtos/DishDto.cs
@@ -0,0 +1,27 @@
+﻿using Restaurants.Application.Restaurants.Dtos;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Application.Dishes.Dtos;
+
+public class DishDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public decimal Price { get; set; }
+    public int? KiloCalories { get; set; }
+
+    public int RestaurantId { get; set; }
+
+    public static DishDto FromEntity(Dish dish)
+    {
+        return new DishDto()
+        {
+            Id = dish.Id,
+            Name = dish.Name,
+            Description = dish.Description,
+            Price = dish.Price,
+            KiloCalories = dish.KiloCalories
+        };
+    }
+}
--- Restaurants.Application/Restaurants/RestaurantsService.cs
+++ Restaurants.Application/Restaurants/RestaurantsService.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
-using Restaurants.Domain.Entities;
+using Restaurants.Application.Restaurants.Dtos; 
 using Restaurants.Domain.Repositories;
 
 namespace Restaurants.Application.Restaurants;
@@ -7,17 +7,21 @@ namespace Restaurants.Application.Restaurants;
 internal class RestaurantsService(IRestaurantsRepository restaurantsRepository,
     ILogger<RestaurantsService> logger) : IRestaurantsService
 {
-    public async Task<IEnumerable<Restaurant>> GetAllRestaurants()
+    public async Task<IEnumerable<RestaurantDto>> GetAllRestaurants()
     {
         logger.LogInformation("Getting all Restaurants");
         var restaurants = await restaurantsRepository.GetAllAsync();
-        return restaurants;
+        var restaurantsDtos = restaurants.Select(RestaurantDto.FromEntity);
+
+        return restaurantsDtos!;
     }
 
-    public async Task<Restaurant?> GetById(int id)
+    public async Task<RestaurantDto?> GetById(int id)
     {
         logger.LogInformation($"Getting restaurant by {id}");
         var restaurant = await restaurantsRepository.GetByIdAsync(id);
-        return restaurant;
+        var restaurantsDto = RestaurantDto.FromEntity(restaurant);
+
+        return restaurantsDto;
     }
 }
