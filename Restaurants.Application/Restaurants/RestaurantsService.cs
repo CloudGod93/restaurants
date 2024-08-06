1d50c84683ef1e8ac8f26b55bc17cc2e4fd6cdf3
Justin Tingley <Jtingley2021@gmail.com>
2024-08-06 16:59:57 -0500

    auto mapper w/POST

--- Restaurants.Application/Restaurants/RestaurantsService.cs
+++ Restaurants.Application/Restaurants/RestaurantsService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.Extensions.Logging;
-using Restaurants.Application.Restaurants.Dtos; 
+using Restaurants.Application.Restaurants.Dtos;
+using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
 
 namespace Restaurants.Application.Restaurants;
@@ -9,6 +10,14 @@ internal class RestaurantsService(IRestaurantsRepository restaurantsRepository,
     ILogger<RestaurantsService> logger,
     IMapper mapper) : IRestaurantsService
 {
+    public async Task<int> Create(CreateRestaurantDto dto)
+    {
+        logger.LogInformation("Creating a new restaurant");
+        var restaurant = mapper.Map<Restaurant>(dto);
+        int id = await restaurantsRepository.Create(restaurant);
+        return id;
+    }
+
     public async Task<IEnumerable<RestaurantDto>> GetAllRestaurants()
     {
         logger.LogInformation("Getting all Restaurants");
